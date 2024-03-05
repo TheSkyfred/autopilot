b25afed58f718c2786ce8e9bb1f218e313b8e063
Theskyfred <theskyfred@gmail.com>
2024-03-05 22:04:27 +0100

    erase

--- include/Compass.h
+++
@@ -1,8 +0,0 @@
-#ifndef COMPASS_H_
-#define COMPASS_H_
-
-void Compass_init();
-void Compass_update();
-void displaySensorDetails();
-
-#endif /* COMPASS_H_ */
--- include/Servos.h
+++
@@ -1,9 +0,0 @@
-#ifndef SERVOS_H_
-#define SERVOS_H_
-
-
-//Functions :
-void Servos_init();
-void Servos_update();
-
-#endif /* SERVOS_H_ */
